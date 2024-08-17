bcaeea3112b55d24ced612bc6f6f6810b7760d3b
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-08-17 15:29:22 +0530

    Added booking logic with transactions

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,22 +1,32 @@
 const axios = require("axios")
-
-const { BookingRepository } = require('../repositories')
 const db = require("../models");
 const { serverConfig } = require("../config");
 const AppError = require("../utils/errors/app-error");
 const { StatusCodes } = require("http-status-codes");
+const { BookingRepository } = require('../repositories')
+const bookingRepository = new BookingRepository();
 
 async function createBooking(data) {
-    return new Promise((reject, resolve)=>{
-        const result = db.sequelize.transaction(async function bookingImpl(t) {
-            const flight = await axios.get(`${serverConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`)           
-            const flightData = flight.data.data
-            if(data.noOfSeats > flightData.totalSeats){
-                reject(new AppError('Not enough seats available', StatusCodes.BAD_REQUEST))
-            }
-            resolve(true)
-        })
-    })
+    const transaction = await db.sequelize.transaction();
+    try {
+        const flight = await axios.get(`${serverConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data;
+        if(data.noOfSeats > flightData.totalSeats) {
+            throw new AppError('Not enough seats available', StatusCodes.BAD_REQUEST);
+        }
+        const totalBillingAmount = data.noOfSeats * flightData.price;
+        const bookingPayload = {...data, totalCost: totalBillingAmount};
+        const booking = await bookingRepository.create(bookingPayload, transaction);
+
+        await axios.patch(`${serverConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+            seats: data.noOfSeats
+        });
+        await transaction.commit();
+        return booking;
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
 }
 module.exports = {
     createBooking
